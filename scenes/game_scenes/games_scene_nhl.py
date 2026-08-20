e7771e4c92c466e10b13da954ed8589a2ae99a4e
jawhett1 <jawhett1@gmail.com>
2026-08-20 07:49:23 -0700

    fix: Python 3.10 f-string syntax in games_scene_nhl.py

--- scenes/game_scenes/games_scene_nhl.py
+++ scenes/game_scenes/games_scene_nhl.py
@@ -360,7 +360,7 @@ class NHLGamesScene(GamesScene):
 
         # Otherwise, we're in 2OT, or later. Calculate the number of OT periods and add that to the image.
         elif game['period_type'] == 'OT':
-            per = f'{game['period_num'] - 3}{game['period_type']}'
+            per = f"{game['period_num'] - 3}{game['period_type']}"
             self.draw['centre'].text((1, -1), per, font=self.FONTS['med'], fill=self.COLOURS['white'])
 
 
